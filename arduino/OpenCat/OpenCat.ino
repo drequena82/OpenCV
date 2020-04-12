69e02601b078d29ddbcdb011fb139b14edb53710
drequena82 <37770831+drequena82@users.noreply.github.com>
2020-04-12 13:17:55 +0200

    Añadido el sensor de ultrasonido

--- arduino/OpenCat/OpenCat.ino
+++ arduino/OpenCat/OpenCat.ino
@@ -25,8 +25,18 @@ unsigned int pataTraseraDer[] = {6,7};
 unsigned int cuello[] = {14,15};
 unsigned int cola = 13;
 
+sensors_event_t giro;
+
+const int trigger = 2;   //Pin digital 2 para el Trigger del sensor
+const int echo = 3;   //Pin digital 3 para el Echo del sensor
+
 void setup(void) {
 
+  //Incializacion del sensor de ultrasonido
+  pinMode(trigger, OUTPUT); //pin como salida
+  pinMode(echo, INPUT);  //pin como entrada
+  digitalWrite(trigger, LOW);//Inicializamos el pin con 0
+  
   //Inicialización de los servos
   servos.begin();  
   servos.setPWMFreq(60); //Frecuecia PWM de 60Hz o T=16,66ms
@@ -38,7 +48,7 @@ void setup(void) {
   
   // Try to initialize!
   if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
+    Serial.println("Failed to find MPU6050 chip.");
     while (1) {
       delay(10);
     }
@@ -46,76 +56,22 @@ void setup(void) {
   
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-    case MPU6050_RANGE_2_G:
-      Serial.println("+-2G");
-      break;
-    case MPU6050_RANGE_4_G:
-      Serial.println("+-4G");
-      break;
-    case MPU6050_RANGE_8_G:
-      Serial.println("+-8G");
-      break;
-    case MPU6050_RANGE_16_G:
-      Serial.println("+-16G");
-      break;
-  }
-  
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-    case MPU6050_RANGE_250_DEG:
-      Serial.println("+- 250 deg/s");
-      break;
-    case MPU6050_RANGE_500_DEG:
-      Serial.println("+- 500 deg/s");
-      break;
-    case MPU6050_RANGE_1000_DEG:
-      Serial.println("+- 1000 deg/s");
-      break;
-    case MPU6050_RANGE_2000_DEG:
-      Serial.println("+- 2000 deg/s");
-      break;
-  }
 
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-    case MPU6050_BAND_260_HZ:
-      Serial.println("260 Hz");
-      break;
-    case MPU6050_BAND_184_HZ:
-      Serial.println("184 Hz");
-      break;
-    case MPU6050_BAND_94_HZ:
-      Serial.println("94 Hz");
-      break;
-    case MPU6050_BAND_44_HZ:
-      Serial.println("44 Hz");
-      break;
-    case MPU6050_BAND_21_HZ:
-      Serial.println("21 Hz");
-      break;
-    case MPU6050_BAND_10_HZ:
-      Serial.println("10 Hz");
-      break;
-    case MPU6050_BAND_5_HZ:
-      Serial.println("5 Hz");
-      break;
-  }
 
-  Serial.println("");
-
+  //Posiciones iniciales de los servos (patas estiradas totalmente).
   iniciarMotores();
   
-  delay(100);
+  delay(1000);
+
+  sentado();
+
+  delay(1000);
 }
 
 //Actualizar la posicion del servo, en funcion del tipo de servo, tipo 1 o tipo 2 (hay 2 clases de servos)
-void setServo(uint8_t n_servo, int anguloFin, int type){
+void setServo(uint8_t n_servo, unsigned int anguloFin,unsigned int type){
   if(type == SERVO_TYPE1){
     setServo(n_servo,anguloFin,POS0_TYPE1,POS180_TYPE1);
   }else{
@@ -124,7 +80,7 @@ void setServo(uint8_t n_servo, int anguloFin, int type){
 }
 
 //Actualiza la posicion del servo
-void setServo(int n_servo, int anguloFin,int pos0,int pos180) {
+void setServo(unsigned int n_servo,unsigned int anguloFin,unsigned int pos0,unsigned int pos180) {
   int dutyFin;
   dutyFin=map(anguloFin,0,180,pos0, pos180);
   
@@ -142,6 +98,8 @@ pata trasera derecha      <- posicion inicial 0 grados
 
 //Inicializa los servos a la posicion 0
 void iniciarMotores(){
+  giro = getRotation();
+  
   setServo(cola,0,SERVO_TYPE2);
   setServo(pataDelanteraIzq[0],0,SERVO_TYPE1);
   setServo(pataDelanteraIzq[1],0,SERVO_TYPE2);
@@ -155,19 +113,26 @@ void iniciarMotores(){
 
 //Sentados
 void sentado(){
+  giro = getRotation();
+  
   setServo(cola,0,SERVO_TYPE2);
   setServo(pataDelanteraIzq[0],135,SERVO_TYPE1);
-  setServo(pataDelanteraIzq[1],135,SERVO_TYPE2);
-  setServo(pataTraseraIzq[0],45,SERVO_TYPE1);
+  setServo(pataDelanteraIzq[1],0,SERVO_TYPE2);
+  
+  setServo(pataTraseraIzq[0],90,SERVO_TYPE1);
   setServo(pataTraseraIzq[1],45,SERVO_TYPE2);
+  
   setServo(pataDelanteraDer[0],45,SERVO_TYPE1);
-  setServo(pataDelanteraDer[1],45,SERVO_TYPE2);
-  setServo(pataTraseraDer[0],135,SERVO_TYPE1);
+  setServo(pataDelanteraDer[1],180,SERVO_TYPE2);
+  
+  setServo(pataTraseraDer[0],90,SERVO_TYPE1);
   setServo(pataTraseraDer[1],135,SERVO_TYPE2);
 }
 
 //Movimiento de los servos 
 void plantado(){
+  giro = getRotation();
+  
   setServo(cola,90,SERVO_TYPE2);
   setServo(pataDelanteraIzq[0],90,SERVO_TYPE1);
   setServo(pataDelanteraIzq[1],90,SERVO_TYPE2);
@@ -182,28 +147,38 @@ void plantado(){
 
 //Para caminar
 void marchaIzquierda(){
+  giro = getRotation();
+  
   setServo(cola,70,SERVO_TYPE2);
   setServo(pataDelanteraIzq[0],60,SERVO_TYPE1);
   setServo(pataDelanteraIzq[1],60,SERVO_TYPE2);
-  setServo(pataTraseraIzq[0],120,SERVO_TYPE1);
-  setServo(pataTraseraIzq[1],120,SERVO_TYPE2);
+  setServo(pataTraseraIzq[0],60,SERVO_TYPE1);
+  setServo(pataTraseraIzq[1],60,SERVO_TYPE2);
+
+  delay(100);
+  
   setServo(pataDelanteraDer[0],60,SERVO_TYPE1);
   setServo(pataDelanteraDer[1],60,SERVO_TYPE2);
-  setServo(pataTraseraDer[0],120,SERVO_TYPE1);
-  setServo(pataTraseraDer[1],120,SERVO_TYPE2);
+  setServo(pataTraseraDer[0],60,SERVO_TYPE1);
+  setServo(pataTraseraDer[1],60,SERVO_TYPE2);
 }
 
 //Para caminar
 void marchaDerecha(){
+  giro = getRotation();
+  
   setServo(cola,110,SERVO_TYPE2);
   setServo(pataDelanteraIzq[0],120,SERVO_TYPE1);
   setServo(pataDelanteraIzq[1],120,SERVO_TYPE2);
-  setServo(pataTraseraIzq[0],60,SERVO_TYPE1);
-  setServo(pataTraseraIzq[1],60,SERVO_TYPE2);
+  setServo(pataTraseraIzq[0],120,SERVO_TYPE1);
+  setServo(pataTraseraIzq[1],120,SERVO_TYPE2);
+
+  delay(100);
+  
   setServo(pataDelanteraDer[0],120,SERVO_TYPE1);
   setServo(pataDelanteraDer[1],120,SERVO_TYPE2);
-  setServo(pataTraseraDer[0],60,SERVO_TYPE1);
-  setServo(pataTraseraDer[1],60,SERVO_TYPE2);
+  setServo(pataTraseraDer[0],120,SERVO_TYPE1);
+  setServo(pataTraseraDer[1],120,SERVO_TYPE2);
 }
 
 //Funcion que recoge el objeto de posiciones del giroscopio
@@ -216,7 +191,6 @@ sensors_event_t getRotation(){
   mpu.getEvent(&a, &g, &temp);
 
   /* Print out the values */
-  /*
   Serial.print("Acceleration X: ");
   Serial.print(a.acceleration.x);
   Serial.print(", Y: ");
@@ -224,7 +198,7 @@ sensors_event_t getRotation(){
   Serial.print(", Z: ");
   Serial.print(a.acceleration.z);
   Serial.println(" m/s^2");
-  */
+  
   Serial.print("Rotation X: ");
   Serial.print(g.gyro.x);
   Serial.print(", Y: ");
@@ -242,15 +216,37 @@ sensors_event_t getRotation(){
   return g;
 }
 
-void loop() {
-  //Recogemos el estado de posicion del giroscopio
-  sensors_event_t giro = getRotation();
-  
-  //sentado();
+long getDistancia(){
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
 
-  delay(1500);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(trigger, LOW);
   
-  plantado();
+  t = pulseIn(echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
   
-  delay(1500);
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(100);          //Hacemos una pausa de 100ms
+}
+
+void loop() {
+  //Va a caminar hasta que llegue a 15 cm de un objeto.
+  for(int i=0;i<10;i++){
+    if(getDistancia() > 15){
+      marchaIzquierda();
+      delay(500);
+      marchaDerecha();
+      delay(500);
+    }else{
+      plantado();
+    }
+  }
+  
+  delay(2000);
+
 }
